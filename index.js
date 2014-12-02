7345dba2558f76993fdac9eb9d3844eefe18bd7f
Jeremy Kuzub <jeremy@jufaintermedia.com>
2014-12-02 18:07:58 -0500

    test of request module of JSON repsonse URL

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,8 +8,24 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello RichterSales!');
+  // get orders:
+    urderUrl = 'https://eb269bfed4883478fcf75acac385014d:b4baf3405ca909e683fb80892511a914@jufa-development-shop.myshopify.com/admin/orders.json';
+    retrieveOrders(orderUrl);
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
+app.retrieveOrders(url){
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            response.send('Orders Retrieved:\n---\n'+body) // Print the json response
+        }
+    })
+}
